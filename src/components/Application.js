b526c15e05c038ac3150de936e04d22b030609d0
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-01-31 12:34:38 -0500

    Update days to fetch data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,68 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "components/Application.scss";
-import DayList from "components/DayList"
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 // Mock Data
-const days = [
-  {
+
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [ day, setDay ] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days').then((response) => {
+      const days = []
+
+      for (let day of response.data) {
+        days.push(day);
+      }
+      setDays(days);
+    })
+  }, [])
+
+const appointmentsArray = Object.values(appointments).map((appointment, index) => <Appointment key={appointment.id} {...appointment}/>);
+
+ 
   return (
     <main className="layout">
       <section className="sidebar">
@@ -45,7 +84,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        <ul>{appointmentsArray}<Appointment key="last" time="5pm" /></ul>
       </section>
     </main>
   );
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,7 +3,7 @@ import DayListItem from 'components/DayListItem';
 
 export default function DayList(props) {
 
-  const dayListItemArray = props.daysArray.map((day) =>
+  const dayListItemArray = props.days.map((day) =>
       <DayListItem name={day.name}  spots={day.spots} setDay={props.onChange} key={day.id} selected={day.name === props.value}/>
   );
 
