c5a512335a95eeab3fbeb1867dca03c351ee6da9
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-01-31 16:18:16 -0500

    Set up state as an object

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export default function getAppointmentsForDay(state, currentDay) {
+  const appointments = [];
+
+  for (let day of state.days) {
+    if (day.name === currentDay) {
+      for (let appointment of day.appointments) {
+        appointments.push(state.appointments[appointment])
+      }
+    }
+
+  }
+  return appointments;
+}
