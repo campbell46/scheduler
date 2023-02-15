2e5c338792760b929ba5cbd7b790da582dfae2f6
Sarah Campbell <sarahcampbell4646@gmail.com>
2023-02-15 16:03:42 -0500

    Add tests

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 import 'components/InterviewerList.scss';
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const interviewerArray = props.interviewers;
-
 
   const intListArray = interviewerArray.map((person) => 
     <InterviewerListItem name={person.name} avatar={person.avatar} key={person.id} setInterviewer={() => props.onChange(person.id)} selected={props.value === person.id}/>
@@ -17,4 +17,8 @@ export default function InterviewerList(props) {
   <ul className="interviewers__list">{intListArray}</ul>
 </section>
   );
-}
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
